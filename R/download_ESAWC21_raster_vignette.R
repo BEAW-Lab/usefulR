08a831de4c87121652eddd7bc302826e42e492f7
PabloCapilla <pacapilla@gmail.com>
2025-11-14 12:33:20 +0100

    new example data

--- R/download_ESAWC21_raster_vignette.R
+++ R/download_ESAWC21_raster_vignette.R
@@ -1,8 +1,7 @@
-#' Download ESA WorldCover 2021 data
+#' Download ESA WorldCover 2021 data for package vignette
 #'
 #' @description
-#' This helper function downloads a land cover raster from ESA WorldCover 2021.
-#' It stores it locally, so it needs to be downloaded only once.
+#' This helper function downloads a land cover raster from ESA WorldCover 2021 to use in the package vignette.
 #'
 #' @param local_dir Path to directory where the file should be saved.
 #'   If NULL (default), a system cache directory is used.
@@ -12,12 +11,12 @@
 #' @return The full path to the downloaded raster file.
 #' @examples
 #' \dontrun{
-#' TBC
+#' download_ESAWC21_raster_vignette(local_dir = "data_vignette/")
 #' }
 #' @export
-download_ESAWC21_raster <- function(local_dir = NULL,
-                                    file_name = "LC_raster.tif",
-                                    force = FALSE) {
+download_ESAWC21_raster_vignette <- function(local_dir = './data_vignette',
+                                             file_name = "LC_raster_example.tif",
+                                             force = FALSE) {
   requireNamespace("utils")
   requireNamespace("rappdirs")
   
@@ -30,7 +29,7 @@ download_ESAWC21_raster <- function(local_dir = NULL,
   options(timeout = max(600, getOption("timeout")))
   
   # --- Remote source (Dropbox direct link) ---
-  url <- "https://www.dropbox.com/scl/fi/100yws6j6v1bburss7t6b/ESA21_WLC_raster.tif?rlkey=yl8lyidmenr33lb6gh0ugajva&dl=1"
+  url <- "https://www.dropbox.com/scl/fi/g2j8f5jwpnp124xlopfdj/ESA_WorldCover_10m_2021_V200_N39W006_Map.tif?rlkey=3obz9tvp6zds78kcbvq9mcbv5&dl=1"
   
   # --- Determine where to store file ---
   if (is.null(local_dir)) {
@@ -43,7 +42,7 @@ download_ESAWC21_raster <- function(local_dir = NULL,
   
   # --- Download if missing or forced ---
   if (!file.exists(local_file) || force) {
-    message("Downloading ", file_name, " (~630 MB)... please wait.")
+    message("Downloading ", file_name, " (~100 MB)... please wait.")
     tryCatch({
       utils::download.file(url, destfile = local_file, mode = "wb", quiet = FALSE)
       message("✅ Download complete. File saved to: ", normalizePath(local_file))
@@ -56,8 +55,8 @@ download_ESAWC21_raster <- function(local_dir = NULL,
   
   # --- Verify download integrity ---
   size <- file.info(local_file)$size
-  if (is.na(size) || size < 500e6) {
-    warning("File may be incomplete (size < 500 MB). Try re-downloading with force = TRUE.")
+  if (is.na(size) || size < 150e6) {
+    warning("File may be incomplete (size < 100 MB). Try re-downloading with force = TRUE.")
   }
   
   return(local_file)
